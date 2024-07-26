68d0dfa7b3e6ae21187b4555aff34bc1cdc95a1b
Craig Jarvis <craig.jarvis@gmail.com>
2024-07-26 17:40:02 +1000

    Some work

--- EmbyPluginAppriseApi/Plugin.cs
+++ EmbyPluginAppriseApi/Plugin.cs
@@ -1,44 +1,44 @@
-﻿namespace EmbyPluginAppriseApi
+﻿using System;
+using System.IO;
+
+using MediaBrowser.Common;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Controller.Plugins;
+using MediaBrowser.Model.Drawing;
+using MediaBrowser.Model.Logging;
+
+namespace EmbyPluginAppriseApi
 {
-    using System;
-    using System.IO;
-
-    using MediaBrowser.Common;
-    using MediaBrowser.Common.Plugins;
-    using MediaBrowser.Controller.Plugins;
-    using MediaBrowser.Model.Drawing;
-    using MediaBrowser.Model.Logging;
-
     /// <summary>
     /// The plugin.
     /// </summary>
     public class Plugin : BasePluginSimpleUI<PluginOptions>, IHasThumbImage
     {
         /// <summary>The Plugin ID.</summary>
-        private readonly Guid id = new Guid("00000000-0000-0000-0000-000000000000"); // << Generate one: Tools >> Create GUID
+        private readonly Guid id = new Guid("9f19e7af-0cd0-4370-967c-3a87fb63a7d2");
 
-        private readonly ILogger logger;
+        private readonly ILogger _logger;
 
         /// <summary>Initializes a new instance of the <see cref="Plugin" /> class.</summary>
         /// <param name="applicationHost">The application host.</param>
         /// <param name="logManager">The log manager.</param>
         public Plugin(IApplicationHost applicationHost, ILogManager logManager) : base(applicationHost)
         {
-            this.logger = logManager.GetLogger(this.Name);
-            this.logger.Info("My plugin ({0}) is getting loaded", this.Name);
+            _logger = logManager.GetLogger(this.Name);
+            _logger.Info("My plugin ({0}) is getting loaded", this.Name);
         }
 
         /// <summary>Gets the description.</summary>
         /// <value>The description.</value>
-        public override string Description => "XVXVXVXVXVXVX";
+        public override string Description => "Get notified about server events via an Apprise API instance";
 
         /// <summary>Gets the unique id.</summary>
         /// <value>The unique id.</value>
-        public override Guid Id => this.id;
+        public override Guid Id => id;
 
         /// <summary>Gets the name of the plugin</summary>
         /// <value>The name.</value>
-        public override sealed string Name => "´XVXVXVXVXVXVX";
+        public override sealed string Name => "´AppriseAPI Notifications";
 
         /// <summary>Gets the thumb image format.</summary>
         /// <value>The thumb image format.</value>
@@ -54,7 +54,7 @@
 
         protected override void OnOptionsSaved(PluginOptions options)
         {
-            this.logger.Info("My plugin ({0}) options have been updated.", this.Name);
+            _logger.Info("My plugin ({0}) options have been updated.", this.Name);
         }
     }
 }
